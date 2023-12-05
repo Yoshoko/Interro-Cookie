adc5c42257a793e8539355bb55f8e494e5c740c3
AymericMartinez <aymeric.martinez@esd-mail.com>
2023-12-05 16:26:06 +0100

    ajout de ce que j'ai pu

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const btnCookie = document.getElementById("btnCookie")
+const NbCookies = document.getElementById("btnCookies")
+const NbDollars = document.getElementById("btnDollars")
+const NbGrandma = document.getElementById("btnGrandma")
+const BuyGrandma = document.getElementById("BtnGrandma")
+
+
+btnCookie.onclick = function () {
+    NbCookies.innerHTML
+}
+
+btnCookie.onclick = 
